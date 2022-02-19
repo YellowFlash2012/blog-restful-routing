cd9cbce9868dc1b5fc86fe97c74d941e650eb331
YellowFlash2012 <cyclop9@hotmail.com>
2022-02-19 08:59:08 +0100

    fixed the config file to take into account some postgres syntax

--- tentenblog/__init__.py
+++ tentenblog/__init__.py
@@ -15,17 +15,18 @@ from flask_ckeditor import CKEditor
 # flask security
 from flask_login import LoginManager
 
-# from tentenblog.config import Config
+from tentenblog.config import Config
 
 ## Delete this code:
 # import requests
 # posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
 app = Flask(__name__)
+app.config.from_object(Config)
+# app.config['SECRET_KEY']=os.environ.get('SECRET_KEY')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-app.config['SECRET_KEY']=os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # app.config.from_object(Config())
 ckeditor = CKEditor(app)
--- tentenblog/config.py
+++ tentenblog/config.py
@@ -5,7 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
